2322e126c58d5c69d1e634258110d8673da6cd38
ZaitsevaDiana <150392034+ZaitsevaDiana@users.noreply.github.com>
2024-04-14 22:02:43 +0300

    edit

--- app/src/main/java/com/example/bookshelf/ui/screens/menu_screen/MenuScreen.kt
+++ app/src/main/java/com/example/bookshelf/ui/screens/menu_screen/MenuScreen.kt
@@ -1,6 +1,5 @@
 package com.example.bookshelf.ui.screens.menu_screen
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
